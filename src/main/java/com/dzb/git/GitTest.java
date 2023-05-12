ac1264c2afa0359898a27cf4dbed031f5b1822ae
billy <1252615842@qq.com>
2023-05-12 11:35:31 +0800

    second commit

--- src/main/java/com/dzb/git/GitTest.java
+++ src/main/java/com/dzb/git/GitTest.java
@@ -8,6 +8,7 @@ package com.dzb.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello Git!!!");
+        System.out.println("hello Git1!!!");
+        System.out.println("hello Git2!!!");
     }
 }
